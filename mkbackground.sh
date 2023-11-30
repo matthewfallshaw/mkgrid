d8224d4d9fcc8e0f4998ad7a6d8797713b0c6ce4
Matthew Fallshaw <m@fallshaw.me>
2023-11-30 21:50:46 +1100

    An actually functioning start.

--- mkbackground.sh
+++ mkbackground.sh
@@ -1,30 +1,95 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Define dimensions
-width=2560
-height=1600
+# Usage: ./this_script.sh width height main_grid_size sub_grid_factor background_color line_color
+# Example: ./this_script.sh 1920 1080 100 2 "rgb(0,77,129)" "white"
 
-# Define colors
-background_color="rgb(0,77,129)"
-line_color="white"
+# Get script parameters
+width=${1:-2560}
+height=${2:-1600}
+main_grid_size=${3:-100}
+sub_grid_factor=${4:-2} # Sub-grid factor relative to main grid (0.5, 1, 2, etc.)
+background_color=${5:-"rgb(0,77,129)"}
+line_color=${6:-"white"}
+alpha=${7:-0.5} # Alpha value for sub grid lines
+strokewidth=${8:-1}
+sub_grid_size=$(( main_grid_size / sub_grid_factor ))
+
+current_step=0
+total_main_lines=$(( (width / main_grid_size + (height / main_grid_size)) * 2 - 4 ))
+total_sub_lines=$(( (width / sub_grid_size + (height / sub_grid_size)) * 2 * (sub_grid_factor - 1) / sub_grid_factor - 4 ))
+total_steps=$(( total_main_lines + total_sub_lines ))
+
+# Check for ImageMagick's 'convert' command
+if ! command -v convert &>/dev/null; then
+    echo "convert command not found. Please ensure ImageMagick is installed or run 'nix-shell' if you are using a Nix environment."
+    exit 1
+fi
+
+# Function to draw the grid, skipping lines that overlap with the main grid
+function draw_grid() {
+    local -i size=$1
+    local color=$2
+    local increment=$size
+    local -i skip_factor=$3
+    local -i strokewidth=$4
+
+    local commands=()
+
+for ((i = increment; i < width; i += increment)); do
+    (( skip_factor > 0 && i % (increment * skip_factor) == 0 )) && continue
+    commands+=("-draw")
+    commands+=("line $i,0 $i,$height")
+done
+
+for ((i = increment; i < height; i += increment)); do
+    (( skip_factor > 0 && i % (increment * skip_factor) == 0 )) && continue
+    commands+=("-draw")
+    commands+=("line 0,$i $width,$i")
+done
+
+    # Apply all drawing commands at once
+    convert background.png -fill none -stroke "$color" -strokewidth "$strokewidth" "${commands[@]}" background.png
+    update_progress ${#commands[@]}
+}
+
+function convert_rgb_to_rgba() {
+    local rgb_color=$1
+    local alpha_value=$2
+    # Replace 'rgb' with 'rgba' and append the alpha value before the closing parenthesis
+    local rgba_color=$(echo "$rgb_color" | sed -e 's/rgb/rgba/' -e "s/)/,${alpha_value})/")
+    echo "$rgba_color"
+}
+
+# Function to draw a progress bar
+function draw_progress_bar() {
+    local -i step=$1
+    local -i total=$2
+    local -i percentage=$((step * 100 / total))
+    local -i bar_length=50
+    local -i filled_length=$((percentage * bar_length / 100))
+    local bar=$(printf "%-${bar_length}s" "=")
+    echo -ne "\r[${bar:0:$filled_length}$(printf ' %.0s' $(seq $filled_length $bar_length))] $percentage% completed"
+}
+
+# Function to update progress and draw the progress bar
+function update_progress() {
+    ((current_step++))
+    draw_progress_bar $current_step $total_steps
+}
 
 # Create a background image
-convert -size ${width}x${height} xc:$background_color background.png
+convert -size "${width}x${height}" xc:"$background_color" background.png
 
-# Draw the 100px grid
-for ((i = 100; i < $width; i+=100)); do
-   convert background.png -fill none -stroke $line_color -draw "line $i,0 $i,$height" background.png
-done
-
-for ((i = 100; i < $height; i+=100)); do
-   convert background.png -fill none -stroke $line_color -draw "line 0,$i $width,$i" background.png
-done
+# Draw the main grid
+draw_grid $main_grid_size "$line_color" 0 $strokewidth
 
 # Draw the 50px grid with 50% opacity
-for ((i = 50; i < $width; i+=50)); do
-   convert background.png -fill none -stroke "rgba(255, 255, 255, 0.5)" -draw "line $i,0 $i,$height" background.png
-done
+# Convert line color to RGBA with 0.5 alpha for sub grid
+rgba_line_color=$(convert_rgb_to_rgba "$line_color" $alpha)
 
-for ((i = 50; i < $height; i+=50)); do
-   convert background.png -fill none -stroke "rgba(255, 255, 255, 0.5)" -draw "line 0,$i $width,$i" background.png
-done
+draw_grid $sub_grid_size "$rgba_line_color" $sub_grid_factor
+
+echo # Move to a new line after progress bar completion
+
+echo "Image generated as background.png"
+echo "$line_color || $alpha || $rgba_line_color || "
